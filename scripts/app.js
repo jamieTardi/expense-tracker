4f5f503f7623c0234de7d407b22dc0b5818eb7fe
jamieTardi <jamie.tardi@gmail.com>
2020-09-13 17:47:49 +0100

    added div on click

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
-expenseBtn = document.querySelector('.expense-add')
+const expenseBtn = document.querySelector('.expense-add');
+const inputBoxes = document.querySelectorAll('.input-box');
+const containerDiv = document.querySelector('.container')
+
 
 expenseBtn.addEventListener('click', () => {
-    console.log('i am working')
+   
+    inputBoxes.forEach((box) => {
+        let values = box.value;
+        newExpense = () => {
+            let newDiv = document.createElement('div')
+            let newPara = document.createElement('p')
+            containerDiv.appendChild(newDiv)
+            newDiv.appendChild(newPara)
+            newDiv.classList.add('expense-list') 
+            newPara.classList.add('expense-para')
+            newPara.innerText = values 
+        }
+        newExpense()
+    })
 })
+
